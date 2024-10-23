acf2f3c71ef03d03c8efdaf37ab11b327c5ad785
venkataramana-104 <chekkavenkataramana1@gmail.com>
2024-10-23 14:50:49 +0530

    ramana.zip

--- quickhr104/quickhr104/settings.py
+++ quickhr104/quickhr104/settings.py
@@ -76,15 +76,7 @@ WSGI_APPLICATION = 'quickhr104.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
