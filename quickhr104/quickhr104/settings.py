e8c3a8f98f03fcf83cf44231ba4941940e5c45d1
venkataramana-104 <chekkavenkataramana1@gmail.com>
2024-10-23 14:48:55 +0530

    Update settings.py

--- quickhr104/quickhr104/settings.py
+++ quickhr104/quickhr104/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'Ramana@123',
         'HOST': '127.0.0.1',
-        'PORT': '3307',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
